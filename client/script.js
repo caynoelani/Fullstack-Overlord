baff021cfcf891a6b14d23d436d4504abe2a2d4c
Cayla Bradley <cayla.bradley94@gmail.com>
2022-12-24 11:55:20 -0600

    alternate message rows

--- client/script.js
+++ client/script.js
@@ -1,7 +1,7 @@
-import bot from './assets/bog.svg';
+import bot from './assets/bot.svg';
 import user from './assets/user.svg';
 
-const form = document.querySelector('form');
+const formPrompt = document.querySelector('#form_prompt');
 const chatContainer = document.querySelector('#chat_container');
 
 let loadInterval;
@@ -13,5 +13,66 @@ function loader(element){
     element.textContent += '.';
 
     if (element.textContent === '....') element.textContent = '';
-  }, 300)
+  }, 250)
 }
+
+function typeWriter(element, text) {
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if(index < text.length){
+      element.innerHTML += text.chartAt(index);
+      index++
+    } else {
+      clearInterval(interval);
+    }
+  }, 50);
+}
+
+function generateMessageId(){
+  let randomId = "id" + Math.random().toString(16).slice(2) + Date.now();
+
+  return randomId;
+}
+
+function alternateChatRows(isUser, value, messageId) {
+  return (
+    `
+      <div class="wrapper ${isOverlord && 'chat_overlord'}">
+        <div class="chat">
+          <div className="profile">
+            <img
+              src="${isOverlord ? bot : user}"
+              alt="${isOverlord ? 'bot' : 'user'}"
+            />
+          </div>
+          <div class="message" id=${messageId}>${value}</div>
+        </div>
+      </div>
+    `
+  )
+}
+
+const handleSubmit = async(e) => {
+  e.preventDefault();
+
+  const data = FormData(formPrompt);
+  
+  chatContainer.innerHTML += alternateChatRows(false, data.get('prompt'));
+
+  formPrompt.reset();
+
+  const messageId = generateMessageId();
+  chatContainer.innerHTML += alternateChatRows(true, ' ', messageId);
+  
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const latestMessageDiv = document.getElementById(messageId);
+
+  loader(latestMessageDiv);
+}
+
+formPrompt.addEventListener('submit', handleSubmit);
+formPrompt.addEventListener('keyup', (e) => {
+  if(e.keyCode === 13) handleSubmit(e);
+})
